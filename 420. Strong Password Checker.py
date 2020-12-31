dd55c7c84b6027b54e9c39240ccdc27a7c612061
amruthsrepo <toamruth@gmail.com>
2020-12-31 14:44:49 +0530

    1mt

--- 420. Strong Password Checker.py
+++ 420. Strong Password Checker.py
@@ -3,11 +3,12 @@ class Solution:
         inputLen = len(password)
         if inputLen < 2:
             return 6 - inputLen
-        noCapital, noSmall, noNumber, steps = True, True, True, 0
+        noCapital, noSmall, noNumber, continousReplaced, steps = True, True, True, False, 0
         prev2, prev1, latestIncr, latestIncrI, steps, replacement = '', '', '', 0, 0, 0
         for i in range(inputLen):
             if prev1 == prev2 == password[i] and (i - latestIncrI) > 1:
                 replacement += 1
+                continousReplaced = continousReplaced or latestIncr == password[i]
                 latestIncrI, latestIncr = i+1, password[i]
             prev2, prev1 = prev1, password[i]
             charCode = ord(password[i])
@@ -18,21 +19,23 @@ class Solution:
             elif 47 < charCode < 58:
                 noNumber = False
         steps += int(noCapital) + int(noSmall) + int(noNumber)
-        # lenAfterSteps = steps + inputLen
-        # steps = (steps, replacement)[replacement > steps]
         if inputLen < 6:
             t = abs(replacement - steps)
             return t if t != 0 else t + (6 - inputLen)
         elif inputLen > 20:
             t = (inputLen - 20)
+            if continousReplaced and t > replacement:
+                return t+replacement-int(t/3)-steps
             return t+steps if t > replacement else replacement if steps == 0 else replacement-steps+t
         elif steps == replacement == 0:
             return 0
-        else:
-            return (steps, replacement)[replacement > steps]
+        return (steps, replacement)[replacement > steps]
 
 
 s = Solution()
+print(f'4 : {s.strongPasswordChecker("A1234567890aaabbbbccccc")}')
+print(f'23 : {s.strongPasswordChecker("FFFFFFFFFFFFFFF11111111111111111111AAA")}')
+print(f'8 : {s.strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc")}')
 print(f'8 : {s.strongPasswordChecker("aaaabbbbccccddeeddeeddeedd")}')
 print(f'5 : {s.strongPasswordChecker("aaaaAAAAAA000000123456")}')
 print(f'1 : {s.strongPasswordChecker("aa123")}')
@@ -40,4 +43,3 @@ print(f'0 : {s.strongPasswordChecker("1337C0d3")}')
 print(f'3 : {s.strongPasswordChecker("1111111111")}')
 print(f'2 : {s.strongPasswordChecker("aaa111")}')
 print(f'3 : {s.strongPasswordChecker("aA1")}')
-print(f'8 : {s.strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc")}')
