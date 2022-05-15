8652493609d5f6a82e9a0fa3901c6e3c196241fb
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2022-05-15 15:12:23 +0000

    sub

--- LeetCode/Programs/424. Longest Repeating Character Replace.py
+++ LeetCode/Programs/424. Longest Repeating Character Replace.py
@@ -1,5 +1,60 @@
-import imp
+# from collections import defaultdict
 
+# class Solution(object):
+#     def characterReplacement(self, s, k):
+#         """
+#         :type s: str
+#         :type k: int
+#         :rtype: int
+#         """
+#         lenS = len(s)
+#         if lenS < k:
+#             return lenS
+#         # print(lenS, 'l')
+#         letterCounts = defaultdict(int)
+#         for c in s:
+#             letterCounts[c] += 1
+#         maxRepeatingLetterCount = 0
+#         def refreshMaxRepeatingLetter():
+#             maxCount = 0
+#             maxChar = ''
+#             for c in letterCounts:
+#                 if letterCounts[c] > maxCount:
+#                     maxCount = letterCounts[c]
+#                     maxChar = c
+#             return maxChar, maxCount
+#         maxRepeatingLetter, maxRepeatingLetterCount = refreshMaxRepeatingLetter()
+#         while (lenS-maxRepeatingLetterCount) > k:
+#             print(lenS, maxRepeatingLetterCount, lenS-maxRepeatingLetterCount, k, s)
+#             if letterCounts[s[0]] > letterCounts[s[-1]]:
+#                 letterCounts[s[-1]] -= 1
+#                 if s[-1] == maxRepeatingLetter:
+#                     maxRepeatingLetter, maxRepeatingLetterCount = refreshMaxRepeatingLetter()
+#                 s = s[:-1]
+#             elif letterCounts[s[0]] < letterCounts[s[-1]]:
+#                 letterCounts[s[0]] -= 1
+#                 if s[0] == maxRepeatingLetter:
+#                     maxRepeatingLetter, maxRepeatingLetterCount = refreshMaxRepeatingLetter()
+#                 s = s[1:]
+#             else:
+#                 lCount,rCount = 1,1
+#                 while s[-(rCount + 1)] == s[-1]:
+#                     rCount += 1
+#                 while s[lCount] == s[0]:
+#                     lCount += 1
+#                 if rCount > lCount:
+#                     letterCounts[s[0]] -= 1
+#                     if s[0] == maxRepeatingLetter:
+#                         maxRepeatingLetter, maxRepeatingLetterCount = refreshMaxRepeatingLetter()
+#                     s = s[1:]
+#                 else:
+#                     letterCounts[s[-1]] -= 1
+#                     if s[-1] == maxRepeatingLetter:
+#                         maxRepeatingLetter, maxRepeatingLetterCount = refreshMaxRepeatingLetter()
+#                     s = s[:-1]
+#             lenS -= 1
+#         print(s)
+#         return lenS
 
 class Solution(object):
     def characterReplacement(self, s, k):
@@ -8,27 +63,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        lenS = len(s)
-        if lenS < k:
-            return lenS
-        maxSeqLen = i = 0
-        while i < (lenS-1) and i < (lenS - maxSeqLen - 1) :
-            j = i + 1
-            currSeqLen = 1
-            numReplace = 0
-            while numReplace <= k and j < lenS:
-                currSeqLen += 1
-                if s[j] != s[i]:
-                    numReplace += 1
-                j += 1
-            # currSeqLen -= int(numReplace > k)
-            maxSeqLen = max(maxSeqLen, currSeqLen)
-            if j == lenS:
-                return maxSeqLen
-            i += 1
-            while i < (lenS-1) and s[i] == s[i+1]:
-                i += 1
-        return maxSeqLen
+        counts = {}
+        l = 0
+        maxF = 0
+        res = 0
+        for r in range(len(s)):
+            counts[s[r]] = counts.get(s[r], 0) + 1
+            maxF = [maxF, counts[s[r]]][maxF < counts[s[r]]]
+
+            while (r-l+1) - maxF > k:
+                counts[s[l]] -= 1
+                l += 1
+            
+            res = [res, r-l+1][res < r-l+1]
+        return res
 
 s = Solution()
-print(s.characterReplacement("AABABBA", 1))
+# print(s.characterReplacement("KRSCDCSONAJNHLBMDQGIFCPEKPOHQIHLTDIQGEKLRLCQNBOHNDQGHJPNDQPERNFSSSRDEQLFPCCCARFMDLHADJADAGNNSBNCJQOF", 4))
+print(s.characterReplacement("ABA", 0))
