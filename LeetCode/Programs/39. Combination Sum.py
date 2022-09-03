3f3530c87b96c514a05e03bcabe5243e01de3926
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2022-09-03 02:52:47 -0400

    SUB 39. Combination Sum

--- LeetCode/Programs/39. Combination Sum.py
+++ LeetCode/Programs/39. Combination Sum.py
@@ -1,35 +1,22 @@
 class Solution(object):
-    def combinationSum(self, candidates, target, mainCall = True):
+    def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        if not len(candidates): return []
         candidates = sorted(candidates)
+        return self.combinationSumUtil(candidates, target, [])
+        
+    def combinationSumUtil(self, candidates, target, preList):
         retList = []
-        if target in candidates:
-            retList.append([target])
-            candidates.remove(target)
-        while candidates[-1] >= target and mainCall:
-            candidates.pop()
-        targetList = []
-        targetListSum = 0
-        for n in candidates:
-            targetListSum += n
-            targetList.append(n)
-            if targetListSum == target:
-                retList.append(list(targetList))
-            elif targetListSum > target and (mainCall or len(targetList) != len(candidates)):
-                combi = self.combinationSum(targetList, targetListSum - target, False)
-                for c in combi:
-                    toAdd = list(set(targetList) - set(c))
-                    if (not mainCall or len(c) > 1) and toAdd not in retList:
-                        retList.append(toAdd)
+        for i in range(len(candidates)):
+            if candidates[i] > target:
+                return retList
+            preList.append(candidates[i])
+            if candidates[i] == target:
+                retList.append(list(preList))
+                return retList
+            retList.extend(self.combinationSumUtil(candidates[i:], (target-candidates[i]), list(preList)))
+            preList.pop()
         return retList
-
-s = Solution()
-for o in s.combinationSum([2,3,6,7], 7):
-    print(o)
-# for o in s.combinationSum([1,2,3,4,5,6,7,8,9], 15):
-#     print(o)
